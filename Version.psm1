d37c2740c871bd44d7a6e332209cd990cd99e37e
Schvenn <42421399+Schvenn@users.noreply.github.com>
2025-05-13 11:29:13 -0400

    Update Version.psm1
    Added -quiet flag
    
    Improved pruning logic to keep oldest and newest version of a file from each date, when there are more than 2, before removing oldest versions from the directory altogether, in order to favour saving major revisions over minor ones.

--- Version.psm1
+++ Version.psm1
@@ -1,4 +1,4 @@
-function version ($cmd,[switch]$prune,[switch]$purge,$maxhistory = 10, [switch]$help) {# Keep a historical list of functions and aliases during development, but only if they change.
+function version ($cmd,[switch]$prune,[switch]$purge,$maxhistory = 10, [switch]$quiet, [switch]$help) {# Keep a historical list of functions and aliases during development, but only if they change.
 
 if ($help) {function scripthelp ($section) {# (Internal) Generate the help sections from the comments section of the script.
 ""; Write-Host -ForegroundColor Yellow ("-" * 100); $pattern = "(?ims)^## ($section.*?)(##|\z)"; $match = [regex]::Match($scripthelp, $pattern); $lines = $match.Groups[1].Value.TrimEnd() -split "`r?`n", 2; Write-Host $lines[0] -ForegroundColor Yellow; Write-Host -ForegroundColor Yellow ("-" * 100)
@@ -20,7 +20,7 @@ if (-not $cmd) {Write-Host -f cyan "`nUsage: version `"command`" -purge -prune #
 $backupdirectory = Join-Path (Split-Path $profile) "Archive\Development History\$cmd"; $validatecommand = Get-Command $cmd -ErrorAction SilentlyContinue
 if ($purge) {Remove-Item $backupdirectory -Recurse -Force -ErrorAction SilentlyContinue; Write-Host -f red "`nDirectory for $cmd has been purged.`n"; return}
 if (-not $validatecommand -or $validatecommand.CommandType -notin 'Function','ExternalScript','Alias') {Write-Host -f red "`nInvalid command: $cmd`n"; return}
-$cmddetails = (Get-Command $cmd).definition; $cmdsourceinfo = (Get-Command $cmd).source; $callcmd = (Get-Command $cmd).displayname
+$cmddetails = (Get-Command $cmd).definition; $cmdsourceinfo = (Get-Command $cmd).source; $callcmd = (Get-Command $cmd).displayname; $cmd = $cmd.tolower()
 
 # Append the alias command to the end of the parent function, if the command referenced is an alias.
 if ($validatecommand.CommandType -in 'Alias') {$parentfunction = (Get-Command $cmddetails).definition; $truecommand = "$parentfunction`nsal -Name $cmd -Value $cmddetails"; $cmddetails = $truecommand}
@@ -29,15 +29,22 @@ if ($validatecommand.CommandType -in 'Alias') {$parentfunction = (Get-Command $c
 if ($cmddetails -match '\$script="([^"]+\.ps1)"; & \$script') {$ps1file = $matches[1]
 if ($ps1file -match '(\$\w+)') {$varName = $matches[1].Substring(1); $varValue = (Get-Variable -Name $varName).Value; $ps1file = $ps1file -replace [regex]::Escape($matches[1]), $varValue}
 if (Test-Path $ps1file) {$ps1Content = Get-Content $ps1file -Raw; $cmddetails += "`n" + ("-" * 100) + "`n" + $ps1Content}}
-$filename = "$cmd - $(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').backup"; $backup = Join-Path $backupdirectory $filename; New-Item -ItemType Directory $backupdirectory -Force | Out-Null; $cmddetails | Out-File $backup -Force; ""; Write-Host -f cyan "Command: " -NoNewLine; Write-Host -f yellow $callcmd; Write-Host -f cyan "Source: " -NoNewLine; Write-Host -f yellow $cmdsourceinfo; Write-Host -f yellow ("-"*100); Write-Host -f white $cmddetails"`n"; Write-Host -f yellow ("-"*100); Write-Host -f green "File $backup saved."; Write-Host -f yellow ("-"*100); ""
-if ($prune) {Get-ChildItem -Path $backupdirectory -File | Sort-Object LastWriteTime | Select-Object -First ([math]::Max(0, (Get-ChildItem -Path $backupdirectory -File).Count - $maxhistory)) | Remove-Item -Force; Get-ChildItem -Path $backupdirectory -File; Write-Host -f yellow ("-"*100)}
 
-# Keep only the oldest versions of the command if the SHA256 hash did not change.
-$files = Get-ChildItem -Path $backupdirectory -File; $fileHashes = @()
-foreach ($file in $files) {$hash = Get-FileHash -Path $file.FullName -Algorithm SHA256; $fileHashes += [PSCustomObject]@{File = $file; Hash = $hash.Hash}}
-$duplicateGroups = $fileHashes | Group-Object -Property Hash
-foreach ($group in $duplicateGroups) {if ($group.Count -gt 1) {$oldestFile = $group.Group | Sort-Object LastWriteTime | Select-Object -First 1; $filesToRemove = $group.Group | Where-Object {$_.File.FullName -ne $oldestFile.File.FullName}
-$filesToRemove | ForEach-Object {Remove-Item $_.File.FullName -Force}}}}
+# Write the file.
+$filename = "$cmd - $(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').backup"; $backup = Join-Path $backupdirectory $filename; New-Item -ItemType Directory $backupdirectory -Force | Out-Null; $cmddetails | Out-File $backup -Force 
+
+# Output to screen.
+if (-not $quiet) {Write-Host -f cyan "Command: " -NoNewLine; Write-Host -f yellow $cmd; Write-Host -f cyan "Source: " -NoNewLine; Write-Host -f yellow $cmdsourceinfo; Write-Host -f yellow ("-"*100); Write-Host -f white $cmddetails"`n"; Write-Host -f yellow ("-"*100) 
+Write-Host -f green "File $backup saved."; Write-Host -f yellow ("-"*100); ""}
+if ($quiet) {Write-Host "$backup saved."}
+
+# Pruning.
+$files = Get-ChildItem -Path $backupdirectory -File | Sort-Object Name; $groupedFiles = $files | Group-Object {if ($_ -match '(\d{4}-\d{2}-\d{2})_\d{2}-\d{2}-\d{2}') {return $matches[1]}}; $toRemove = @()
+foreach ($group in $groupedFiles) {$filesInGroup = $group.Group | Sort-Object Name    
+if ($filesInGroup.Count -gt 2) {$toRemove += $filesInGroup[1..($filesInGroup.Count - 2)]}}
+if ($toRemove.Count -gt 0) {$toRemove | ForEach-Object {Remove-Item $_.FullName -Force}}
+$remainingFiles = Get-ChildItem -Path $backupdirectory -File | Sort-Object LastWriteTime; $extraRemovals = $remainingFiles.Count - $maxhistory
+if ($extraRemovals -gt 0) {$oldestFiles = $remainingFiles | Select-Object -First $extraRemovals; $oldestFiles | ForEach-Object {Remove-Item $_.FullName -Force}}}
 
 Export-ModuleMember -Function version
 
@@ -52,7 +59,11 @@ If the user runs version against an alias, the script will backup the logic of t
 
 Also, if the command, or parent command in the case of an alias, is simply a reference to an external script, this function will also append the logic of that script to the bottom of the file, separated by a hyphenated line.
 
+There is also a -quiet option to reduce the screen output.
+
 By default, the script is set to prune older copies after 10 revisions, but this can be modified and you can use the -prune or -purge entries on demand.
+
+The prune uses logical assumptions to determine development dates, by keeping the oldest and latest versions of a command for any single date, before it resorts to pruning the oldest files, thereby increasing the likelihood that major revisions are kept over minor ones.
 
 It also uses intelligent archiving to ensure that new backups are only created if the SHA256 of the new file will be different than an older one, thereby eliminating duplicates and wasted disk space.
 ##>
